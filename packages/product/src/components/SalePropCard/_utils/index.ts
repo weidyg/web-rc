f1b059ca73da215b3a6045f8e67e463b7f2df1ec
WEIDYG <weidyg@163.com>
2024-11-21 09:11:21 +0800

    refactor(product): 优化 SalePropCard 组件的值比较逻辑
    - 更新了 compareValue 函数的参数类型，使用 SalePropValueType 替代 ValueType
    - 优化了值比较逻辑，使用了更准确的类型引用

--- packages/product/src/components/SalePropCard/_utils/index.ts
+++ packages/product/src/components/SalePropCard/_utils/index.ts
@@ -1,5 +1,5 @@
-import { ValueType } from '@web-rc/biz-product';
+import { SalePropValueType } from "..";
 
-export const compareValue = (v1?: ValueType, v2?: ValueType) => {
+export const compareValue = (v1?: SalePropValueType, v2?: SalePropValueType) => {
   return v1?.group?.value == v2?.group?.value && v1?.value == v2?.value;
 };
