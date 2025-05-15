3e04127d4877c798e2c4314da1648ce637a1607c
WEIDYG <weidyg@163.com>
2025-05-15 13:45:14 +0800

    -

--- packages/utils/.fatherrc.ts
+++ packages/utils/.fatherrc.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'father';
 
 export default defineConfig({
+  umd: {
+    name: 'biz-utils',
+    chainWebpack: (memo) => {
+      memo.output.libraryExport('default');
+      return memo;
+    },
+  },
   extends: '../../.fatherrc.base.ts',
 });
